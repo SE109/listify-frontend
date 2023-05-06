ced05b8884d1126bac39a4b500737915297975af
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-05-06 23:04:03 +0700

    commit tạm

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,19 +1,135 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget {
+import '../widgets/add_task_bottomsheet.dart';
+import '../widgets/more_bottom_sheet.dart';
+
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home Page"),),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _showAddTaskBottomSheet(context, 'uid'),
+        label: const Text('Add'),
+        icon: const Icon(Icons.add),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: false,
+            snap: false,
+            elevation: 0,
+            title: Text('Listify'),
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () => _showMoreBottomSheet(context),
+                icon: const Icon(Icons.more_horiz_rounded),
+              )
+            ],
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(75),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  ),
+                  child: Container()
+
+                  // GestureDetector(
+                  //   onTap: () => showSearch(
+                  //     context: context,
+                  //     delegate: TaskSearchDelegate(),
+                  //   ),
+                  //   child: const SearchBar(),
+                  // ),
+                  ),
+            ),
+          ),
+          // if (currentListId == 'today')
+          //   const TodayListBody()
+          // else if (currentListId == 'favorites')
+          //   const FavoritesListBody()
+          // else
+          //   const PersonalListBody()
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> _showMoreBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      enableDrag: false,
+      builder: (context) {
+        return BottomSheet(
+          enableDrag: false,
+          onClosing: () {},
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+            ),
+          ),
+          builder: (BuildContext context) {
+            return Padding(
+              padding: MediaQuery.of(context).viewInsets,
+              child: const MoreBottomSheet(),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<dynamic> _showAddTaskBottomSheet(BuildContext context, String listId) {
+    return showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      enableDrag: false,
+      isScrollControlled: true,
+      builder: (context) {
+        // return Container(
+        //     padding: EdgeInsets.only(
+        //         bottom: MediaQuery.of(context).viewInsets.bottom),
+        //     child: AddTaskBottomSheet(listId: listId));
+        return BottomSheet(
+          enableDrag: false,
+          onClosing: () {},
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+            ),
+          ),
+          builder: (BuildContext context) {
+            return Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: AddTaskBottomSheet(listId: listId),
+            );
+          },
+        );
+      },
     );
   }
 }
---
+++ lib/views/widgets/add_task_bottomsheet.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AddTaskBottomSheet extends StatefulWidget {
+  const AddTaskBottomSheet({
+    Key? key,
+    required this.listId,
+  }) : super(key: key);
+
+  final String listId;
+
+  @override
+  State<AddTaskBottomSheet> createState() => _AddTaskBottomSheetState();
+}
+
+class _AddTaskBottomSheetState extends State<AddTaskBottomSheet> {
+  var isShowDetailField = false;
+
+  late TextEditingController _titleController;
+  late TextEditingController _detailsController;
+
+  DateTime? currentDate;
+  bool onFavorite = false;
+
+  @override
+  initState() {
+    super.initState();
+    currentDate = widget.listId == 'today' ? DateTime.now() : null;
+    onFavorite = widget.listId == 'favorites';
+
+    _titleController = TextEditingController()
+      ..addListener(() {
+        setState(() {});
+      });
+    _detailsController = TextEditingController()
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  _setShowDetailField() => setState(() {
+        isShowDetailField = !isShowDetailField;
+      });
+
+  _setOnFavorite() => setState(() {
+        onFavorite = !onFavorite;
+      });
+
+  _setCurrentDate(DateTime? time) => setState(() {
+        currentDate = time;
+      });
+
+  // _addTask(TasksController controller) {
+  //   return controller.createTask(
+  //     title: _titleController.text,
+  //     details: _detailsController.text,
+  //     time: currentDate,
+  //     onFavorite: onFavorite,
+  //   );
+  // }
+
+  Future<DateTime?> _showDatePicker(BuildContext context) {
+    return showDatePicker(
+      context: context,
+      initialEntryMode: DatePickerEntryMode.calendarOnly,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1970),
+      lastDate: DateTime(3000),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              controller: _titleController,
+              autofocus: true,
+              textInputAction: TextInputAction.unspecified,
+              decoration: const InputDecoration(
+                hintText: 'New task',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          isShowDetailField
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextField(
+                    controller: _detailsController,
+                    textInputAction: TextInputAction.unspecified,
+                    decoration: const InputDecoration(
+                      hintText: 'Details',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                )
+              : Container(),
+          currentDate != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: InputChip(
+                    label: Text(
+                      'formatDate(currentDate)',
+                      style: Theme.of(context).textTheme.labelLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                    onDeleted: () => _setCurrentDate(null),
+                  ),
+                )
+              : Container(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    isSelected: isShowDetailField,
+                    onPressed: _setShowDetailField,
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.menu_rounded),
+                  ),
+                  IconButton(
+                    isSelected: currentDate != null,
+                    onPressed: () async {
+                      final time = await _showDatePicker(context);
+                      _setCurrentDate(time);
+                    },
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.today_rounded),
+                  ),
+                  IconButton(
+                    isSelected: onFavorite,
+                    onPressed: _setOnFavorite,
+                    padding: EdgeInsets.zero,
+                    icon: onFavorite
+                        ? const Icon(Icons.star_rounded)
+                        : const Icon(Icons.star_border_rounded),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: _titleController.text.isNotEmpty ||
+                          _detailsController.text.isNotEmpty
+                      ? () {}
+                      : null,
+                  child: const Text('Save'),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+}
