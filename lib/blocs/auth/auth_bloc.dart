ab7d5b3903fc9b3df540065a57b6d0651263f16a
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2023-05-11 19:48:12 +0700

    Auth Bloc

---
+++ lib/blocs/auth/auth_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:listify/repositories/user_repository.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserRepository userRepository;
+  AuthBloc(this.userRepository) : super(AuthUninitialized()) {
+    on<AppStarted>((event, emit) async {
+      final bool hasToken = await userRepository.hasToken();
+      if(hasToken){
+        emit(AuthAuthenticated());
+      }
+      else{
+        emit(AuthUnAuthenticated());
+      }
+    });
+    on<LoggedIn>((event, emit) async {
+      emit(AuthLoading());
+      await userRepository.persisteToken(event.token);
+      emit(AuthAuthenticated());
+    });
+    on<LoggedOut>((event, emit) async {
+      emit(AuthLoading());
+      await userRepository.deleteToken();                           
+      emit(AuthUnAuthenticated());
+    });
+  }
+}
